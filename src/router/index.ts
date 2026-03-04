3d8fdd6f0d15fc79ac066edd9256637610e025ca
Butsayamars <Butsayamart0202@gmail.com>
2026-03-04 22:14:23 +0700

    add logout for admin + edit folder router and flie loginAdmin

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginAdmin from '../page/loginadmin.vue'
 
-// จุดที่ 1: นำเข้าไฟล์ loginadmin.vue จากโฟลเดอร์ page
-import LoginAdmin from '../page/loginadmin.vue' 
+// จุดที่ 1: นำเข้าไฟล์ AdminDashboard ที่เราเพิ่งสร้าง
+import AdminDashboard from '../page/AdminLogout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    // ... (Route หน้าอื่นๆ ที่คุณอาจจะมีอยู่แล้ว เช่น หน้า Home) ...
-
-    // จุดที่ 2: เพิ่ม Route ใหม่สำหรับหน้า Login
     {
-      path: '/login', // กำหนด URL ที่ต้องการให้เข้าถึง (เช่น เปลี่ยนเป็น /login-admin ก็ได้)
+      path: '/login',
       name: 'loginadmin',
       component: LoginAdmin
+    },
+    // จุดที่ 2: เพิ่ม Route ใหม่สำหรับหน้า Dashboard
+    {
+      path: '/Logout', // ตั้งชื่อ URL ว่า /dashboard
+      name: 'AdminLogout',
+      component: AdminDashboard
     }
   ]
 })
